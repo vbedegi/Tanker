c5c1b21e55c743f9218ad3baea075c1068641710
vbedegi <vbedegi@gmail.com>
2011-05-21 22:17:59 +0200

    widget for the masses

--- src/com/vbedegi/tanker/TankerAppWidgetProvider.java
+++ src/com/vbedegi/tanker/TankerAppWidgetProvider.java
@@ -1,6 +1,40 @@
 package com.vbedegi.tanker;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 
 public class TankerAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        int elapsedDays = getElapsedDays(context);
+
+        for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setTextViewText(R.id.widget_text, Integer.toString(elapsedDays));
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    private int getElapsedDays(Context context) {
+        try {
+            Date lastDate = new DatabaseHelper(context).getLastDate();
+            Calendar calendar = Calendar.getInstance();
+            Date now = calendar.getTime();
+            return now.getDate() - lastDate.getDate();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
